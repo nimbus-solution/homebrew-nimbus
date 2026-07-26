c63a6cc653a13e47ee62111f8bbd34b139262397
Oblitus <contact@reneknierim.com>
2026-07-26 21:20:58 +0000

    Fix release URLs in nimbus formula v1.9.2

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -10,7 +10,7 @@ class Nimbus < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Oblitus/nimbus/releases/download/v1.9.2/nimbus_1.9.2_darwin_amd64.tar.gz"
+      url "https://github.com/nimbus-solution/nimbus/releases/download/v1.9.2/nimbus_1.9.2_darwin_amd64.tar.gz"
       sha256 "62c719987516dc5174ac011f4ab9be65227c9e903cefc9ee0b7c531766085f6b"
 
       define_method(:install) do
@@ -18,7 +18,7 @@ class Nimbus < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Oblitus/nimbus/releases/download/v1.9.2/nimbus_1.9.2_darwin_arm64.tar.gz"
+      url "https://github.com/nimbus-solution/nimbus/releases/download/v1.9.2/nimbus_1.9.2_darwin_arm64.tar.gz"
       sha256 "09fadb6f2d57ab5b9ab35396bfcd99085af32f6e03e6ba8058935345956e0634"
 
       define_method(:install) do
@@ -29,14 +29,14 @@ class Nimbus < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Oblitus/nimbus/releases/download/v1.9.2/nimbus_1.9.2_linux_amd64.tar.gz"
+      url "https://github.com/nimbus-solution/nimbus/releases/download/v1.9.2/nimbus_1.9.2_linux_amd64.tar.gz"
       sha256 "6fe9cd21edc77f39cb521a06950230144f8d6ff6980f9d0c22a6d080d4120026"
       define_method(:install) do
         bin.install "nimbus"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Oblitus/nimbus/releases/download/v1.9.2/nimbus_1.9.2_linux_arm64.tar.gz"
+      url "https://github.com/nimbus-solution/nimbus/releases/download/v1.9.2/nimbus_1.9.2_linux_arm64.tar.gz"
       sha256 "3e704b536f896d560c81b5c6b64b57a0bb163a182fc23b8f6dbd92a1126cfd52"
       define_method(:install) do
         bin.install "nimbus"
